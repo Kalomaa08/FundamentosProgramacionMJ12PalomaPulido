8a885a51c3a3f506c0d721589609c02cd5517f08
Kalomaa08 <paloma.pulido@upb.edu.co>
2026-07-28 13:34:59 -0500

    mi primer commit

---
+++ 1. Variables constantes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace _1.Variables_constantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aquí escribo el codigo fuente
+            String nombre = "Paloma Pulido";
+            Console.WriteLine("Bienvenido" + nombre +" al curso de fundamentos de programación");
+        }
+    }
+}
