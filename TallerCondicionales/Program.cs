9a3c3594813e439dcb541eceea92e797374abd1c
Kalomaa08 <paloma.pulido@upb.edu.co>
2026-08-18 13:34:07 -0500

    taller para parcial
    hasta el punto 2

---
+++ TallerCondicionales/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace TallerCondicionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        /*{
+            int numero1 = 0;
+            int numero2 = 0;
+            int numero3 = 0;
+            Console.WriteLine("Por favor ingrese 3 valores diferentes");
+            Console.WriteLine("Ingrese el valor del número 1");
+            numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor del número 2");
+            numero2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor del número 3");
+            numero3 = int.Parse(Console.ReadLine());
+
+            if (numero1 == numero2 || numero1 == numero3 || numero2 == numero3)
+            {
+                Console.WriteLine(" Error, todos los valores tienen que ser diferentes");
+            }
+            else
+            {
+                if (numero1 > numero2 && numero1 > numero3)
+                {
+                    Console.WriteLine("El número 1 es el mayor");
+                }
+                else
+                {
+                    if (numero2 > numero3)
+                    {
+                        Console.WriteLine("El número 2 es el mayor");
+                    }
+                    else
+                    {
+                        Console.WriteLine("El número 3 es el mayor");
+                    }
+                }
+            }
+        }*/
+        {
+            int municion = 0;
+            int invensible = 0;
+            Console.WriteLine(" el personaje cuenta con municion ? si o no");
+            
+        }
+    }
+}
