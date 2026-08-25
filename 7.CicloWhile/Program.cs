3b921d7069ad825acc38b2dd6561dc8df88685ee
Kalomaa08 <paloma.pulido@upb.edu.co>
2026-08-25 13:29:37 -0500

    7

---
+++ 7.CicloWhile/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace _7.CicloWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*//algoritmo qu genere los primeros 5 #s entertos + y realice y muestree sus suma
+            int contador = 1;
+            int acumulador = 1;
+            
+            while (contador < 5)
+            {
+                contador++;
+                acumulador = acumulador + contador;
+            }
+            Console.WriteLine($"la suma de los 5 primeros numeros es:{acumulador}");
+*/
+
+
+            // algoritmo ue calcule el factorial de un numero dado por el usuario 
+
+        }
+    }
+}
