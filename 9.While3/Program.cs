62e879b33d6050549d91ed512625c691b73edfc3
Kalomaa08 <paloma.pulido@upb.edu.co>
2026-08-27 13:30:58 -0500

    9.

---
+++ 9.While3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+
+
+namespace _9.While3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        /*{
+            int positivo = 0;
+            int negativo = 0;
+            int cero = 0;
+            int total = 0;
+            int contador = 0;
+            int numero = 0;
+            
+
+            Console.WriteLine("Cuantos numero va a ingresar");
+            total = int.Parse(Console.ReadLine());
+            
+            while (contador < total)
+            {
+                contador++;
+                Console.WriteLine("ingrese el número");
+                numero = int.Parse(Console.ReadLine());
+            }
+            if (numero ==0)
+            {
+                cero++;
+            }
+            else
+            {
+                if(numero > 0)
+                {
+                    positivo++;
+                }
+                else
+                {
+                    negativo++;
+                }
+            }
+            Console.WriteLine($"Mayor que 0:{positivo}, menores que cero:{negativo}, = a cero {cero}");
+        }*/
+    }   
+
+       
+
+    
+}
