e9e8a37b0da0db3351969303f2f7464e016d2dfd
Kalomaa08 <paloma.pulido@upb.edu.co>
2026-09-15 13:27:36 -0500

    faltan 2 ejercicios

---
+++ 14.ArreglosUnidimensionales/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.ArreglosUnidimensionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* //Arreglos unidimensionales vectores [] 
+             int[] numeros = new int[5];// rango de 5
+             numeros[0]= 10;
+             numeros[1]= 56;
+             numeros[2]= 22;
+             numeros[3]= 45;
+             numeros[4]= 102;
+             // no se puede almacenar mas veztores que el rango 
+             Console.WriteLine($"El numero almacenado en la posicion 3 con indice 2 es:{numeros[2]}");
+             //otra forma de declarar e inicialixar vectores
+             char[] simbolos = new char[] { '#', '&', 'ó', '0', 'a' };
+             bool[] valoresVerdad = { true, false, false, true, true, false, false };
+
+             //recorrer para almacenar datos 
+             string[] nombres = new string[7];
+             for (int i=0;i<7;i++)
+             {
+                 Console.WriteLine($"INGRESE EL NOMBRE PARA P{i + 1} : I{i}");
+                 nombres[i] = Console.ReadLine();
+             }
+             Console.Clear();
+             //Recorrer´para recuperar
+             for(int i=0; i<nombres.Length; i++)
+             {
+                 Console.WriteLine($"{nombres[i]} |");
+             }*/
+
+            
+
+
+        }
+
+    }
+}
