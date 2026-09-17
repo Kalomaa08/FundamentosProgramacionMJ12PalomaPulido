8f6ebb454fff023c81c7efb3b4c62947f6baa1cc
Kalomaa08 <paloma.pulido@upb.edu.co>
2026-09-17 13:24:36 -0500

    taller

---
+++ TallerDeVectores/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace TallerDeVectores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*               const int lol= 16; // ejercicio 1
+                            int[] vector = new int[lol];
+
+                            Console.WriteLine("ingrese 15 números");
+
+                            for (int i = 1; i < lol; i++)
+                            {
+                                Console.Write($"Ingrese el número para la posición [{i}]: ");
+                            while (!int.TryParse(Console.ReadLine(), out vector[i]))
+                            {
+                                Console.Write($"  [Error] Entrada inválida. Ingrese un número entero para [{i}]: ");
+                            }
+
+                            }
+
+                            int maximo = vector[0];
+                            int minimo = vector[0];
+
+                            for (int i = 1; i < lol; i++)
+                            {
+                                if (vector[i] > maximo)
+                                {
+                                    maximo = vector[i];
+                                }
+                                if (vector[i] < minimo)
+                                {
+                                    minimo = vector[i];
+                                }
+                            }
+
+                            Console.WriteLine($"Valor máximo: {maximo}");
+                            Console.WriteLine($"Valor mínimo: {minimo}");*/
+
+            //ejercicio 2
+            
+                
+                int[] v1 = new int[5];
+                int[] v2 = new int[5];
+
+                v1[0] = 10; v2[0] = 10;  
+                v1[1] = 20; v2[1] = 99;  
+                v1[2] = 30; v2[2] = 30;  
+                v1[3] = 40; v2[3] = 40;  
+                v1[4] = 50; v2[4] = 5;   
+
+                
+                int totalIguales = 0;
+
+                for (int i = 0; i < 5; i++)
+                {
+                    if (v1[i] == v2[i])
+                    {
+                        totalIguales++; 
+                    }
+                }
+
+                
+                Console.WriteLine("Cuántos son iguales en la misma posición: " + totalIguales);
+
+                Console.ReadKey();
+           
+        }
+    }
+}
---
+++ TallerDeVectores/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("TallerDeVectores")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TallerDeVectores")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("3344774a-64b8-4e04-a51f-564335b3a636")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
